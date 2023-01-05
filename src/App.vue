49ef4a95ca5703092ce6b655e01b77c6044506a6
Florencia87 <florenciafregossi@gmail.com>
2023-01-05 11:25:43 -0300

    primer proyecto

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="view">
+    <NavBar />
+    <MisTareas />
+  </div>
+</template>
+
+<script>
+import NavBar from './components/NavBar';
+import MisTareas from './components/MisTareas';
+
+export default {
+  name: 'App',
+  components: {
+    NavBar,
+    MisTareas
+  }
+}
+</script>
+
+<style>
+ .view {
+   background-color: pink ;
+   /* background-image: url(""); */
+   height: 100vh;
+
+}
+
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <nav class="navbar bg-dark" data-bs-theme="dark">
+            <div class="container-fluid">
+                 <span class="navbar-brand mb-0 h1">Mis Tareas para el 2023</span>
+            </div>
+        </nav>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: "MiComponente",
+    data() {
+        return{
+            subtitulo: "Florencia Fregossi"
+            }
+        }
+}
+</script>
